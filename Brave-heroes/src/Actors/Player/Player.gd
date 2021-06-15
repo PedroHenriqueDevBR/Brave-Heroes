3dd5abd3f1b60e8717ba0cbf395ec9121cb360ca
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-06-15 16:41:02 -0300

    feat: add timer on game

--- Brave-heroes/src/Actors/Player/Player.gd
+++ Brave-heroes/src/Actors/Player/Player.gd
@@ -9,6 +9,19 @@ var _velocity: = Vector2.ZERO
 export var is_on_water: = false
 var hp: = 2
 var mango_counter: = 0
+var time_left: = 60.0
+
+
+func _ready() -> void:
+	$Info/MangoCounter.text = 'Mangas: ' + String(mango_counter)
+
+
+func _process(delta: float) -> void:
+	time_left -= delta
+	if time_left <= 0:
+		die()
+	var seconds = fmod(time_left, 60)
+	$Info/Timer.text = '%02d' % [seconds]
 
 
 func _physics_process(delta: float) -> void:
@@ -49,8 +62,6 @@ func _physics_process(delta: float) -> void:
 		_velocity.x *= 0.6
 		_velocity.y *= 0.8
 	_velocity = move_and_slide(_velocity, UP)
-	
-	$Info/MangoCounter.text = 'Mangas: ' + String(mango_counter)
 
 
 func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
@@ -83,3 +94,4 @@ func is_player_out_of_the_camera() -> bool:
 
 func get_mango() -> void:
 	mango_counter += 1
+	$Info/MangoCounter.text = 'Mangas: ' + String(mango_counter)
